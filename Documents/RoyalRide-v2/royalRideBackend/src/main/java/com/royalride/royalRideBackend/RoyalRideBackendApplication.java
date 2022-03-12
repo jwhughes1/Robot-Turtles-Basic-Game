c37932c1abb73233614de81a3591d81f111c4432
jwhughes1 <jwhughes@mta.ca>
2022-03-12 13:44:49 -0400

    I made a change

---
+++ Documents/RoyalRide-v2/royalRideBackend/src/main/java/com/royalride/royalRideBackend/RoyalRideBackendApplication.java
@@ -0,0 +1,14 @@
+package com.royalride.royalRideBackend;
+
+//Justin was here
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalRideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalRideBackendApplication.class, args);
+	}
+
+}
---
+++ Documents/RoyalRide-v2/royalRideBackend/src/test/java/com/royalride/royalRideBackend/RoyalRideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royalride.royalRideBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalRideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
